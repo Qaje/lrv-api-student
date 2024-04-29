ed8e7ff3e78b5dd2d45cbbec39517c789315ee45
qaje <javierpal3232@gmail.com>
2024-04-28 23:41:08 -0400

    testing 1 api

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\studentController;
+
+Route::get('/students',[studentController::class, 'index']);
+
+Route::get('/students/{id}', [studentController::class, 'show']);
+
+Route::post('/students', [studentController::class,'store']);
+
+Route::put('/students/{id}', [studentController::class,'update']);
+
+Route::delete('/students/{id}',[studentController::class, 'destroy']);
+
